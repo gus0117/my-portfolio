025899963bde4bc05fdf0d5861ef4da9224e2320
Gustavo Ordoñez <gustavoalberto117@gmail.com>
2023-01-16 20:02:14 -0300

    Agregado estilo typing en js y css

--- js/effects.js
+++ js/effects.js
@@ -4,3 +4,30 @@
     $(this).addClass('active')
     $(this).children('span').addClass('text-green-active')
 }); */
+
+
+//Typing effect
+
+var i = 0;
+var txt = 'Gustavo Ordoñez';
+var speed = 80;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("my-name").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+typeWriter()
+
+//Links
+
+//https://todoxampp.com/efecto-maquina-de-escribir-con-css/
+
+//https://www.w3schools.com/howto/howto_js_typewriter.asp
+
+//IDEA
+
+//HAcer que aparezca un boton que haga que aparezca todo el texto.
